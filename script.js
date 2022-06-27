86eae9348d4260baf09435ce851761cdef902b44
helioramon <helio.alvareng@gmail.com>
2022-06-27 00:12:16 -0300

    data structure using array

---
+++ script.js
@@ -0,0 +1,18 @@
+let itens = ['cadeira', 'notebook', 'fone de ouvido', 'mouse', 'teclado']
+console.log(itens)
+
+let nome = ['Marcos', 'Maria']
+nome.unshift ('Lilian')
+console.log(nome)
+
+nome.pop()
+console.log(nome)
+
+nome.push('Sarah','Fabricio')
+console.log(nome)
+
+nome.shift()
+console.log(nome)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
